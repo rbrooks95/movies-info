935d01807dc447aa688de8b6258a0d9c74df5cc5
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2021-12-14 23:26:28 -0500

    Second commit

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ const onInput = async (event) => {
     <img src="${imgSrc}" />
      ${movie.Title}
     `;
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -55,3 +60,32 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "83071af5",
+      i: movie.imdbID,
+    },
+  });
+
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return ` 
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}"
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div
+      </div>
+    </article>
+  `;
+};
