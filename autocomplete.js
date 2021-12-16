742449859542563e71ad6d7067a48468fbe1a33c
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2021-12-16 16:05:13 -0500

    wrapped up the project it is functional

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,12 @@
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({
+  root,
+  renderOption,
+  onOptionSelect,
+  inputValue,
+  fetchData,
+}) => {
   root.innerHTML = `
-    <label><b> Search for a Movie</b></label>
+    <label><b> Search </b></label>
     <input class="input"/>
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -9,32 +15,28 @@ const createAutoComplete = ({ root }) => {
     </div>
 `;
 
-  const input = document.querySelector("input");
-  const dropdown = document.querySelector(".dropdown");
-  const resultsWrapper = document.querySelector(".results");
+  const input = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
 
   const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
-    if (!movies.length) {
+    const items = await fetchData(event.target.value);
+    if (!items.length) {
       dropdown.classList.remove("is-active");
       return;
     }
     resultsWrapper.innerHTML = "";
     dropdown.classList.add("is-active");
 
-    for (let movie of movies) {
+    for (let item of items) {
       const option = document.createElement("a");
-      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
       option.classList.add("dropdown-item");
-      option.innerHTML = `
-    <img src="${imgSrc}" />
-     ${movie.Title}
-    `;
+      option.innerHTML = renderOption(item);
       option.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
-        input.value = movie.Title;
-        onMovieSelect(movie);
+        input.value = inputValue(item);
+        onOptionSelect(item);
       });
 
       resultsWrapper.appendChild(option);
